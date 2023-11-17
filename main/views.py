998c9dde237c6123e00420b8ac37006c453623f5
vinamyrnauli <vinasllgn@gmail.com>
2023-11-17 21:59:51 +0700

    fix filter json

--- main/views.py
+++ main/views.py
@@ -117,7 +117,7 @@ def logout_user(request):
     return response
 
 def get_product_json(request):
-    product_item = Product.objects.all()
+    product_item = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('json', product_item))
 
 @csrf_exempt
